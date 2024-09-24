3b3ca888d18033e5db177b2c9698eadad867fc1e
Daigo_MB <149451497+diembz@users.noreply.github.com>
2024-09-24 14:14:15 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,16 @@
 variable "moduleconfig" {
-  type = list(object({
-    llanta = string
-    puerta = number
-    motor  = string
-  }))
-  description = "Una lista de objetos que representa varios coches"
+  type = object({
+    instance = list(object({
+      ami = string
+      instance_type = string
+      tags = list(string)
+    }))
+    bucket = list(object({
+      name = string
+      acl = string
+      tags = list(strigs)
+    }))
+  })
+  description = "Un objeto que contiene la configuracion del modulo"
 }
 
-# Definir otras variables para los parámetros del recurso
-variable "ami" {
-  type        = string
-  description = "El ID de la AMI"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "El tipo de instancia de EC2"
-}
-
-variable "nombre" {
-  type        = string
-  description = "El nombre de..."
-}
-
-variable "acl" {
-  type        = string
-  description = "El nombre de..."
-}
