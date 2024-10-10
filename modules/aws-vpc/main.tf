bbbe35852451c2fe2e7fa94e9c52889d9f21a27e
DieMB <diego.mendozab@outlook.com>
2024-10-10 13:34:10 -0600

    Agregando los argumentos necesarios de la VPC

--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -4,43 +4,29 @@ provider "aws" {                       # Configuración del proveedor AWS.
 
 data "aws_region" "current" {} # Data source que obtiene la información de la región actual en AWS
 
-resource "aws_vpc_ipam" "test" { # Crear un VPC IPAM para gestionar direcciones IP en la región especificada.
-  operating_regions {            # Esto ayuda a automatizar la asignación de IPs y evita conflictos.
-    region_name = var.vpcconfig.vpc[0].region
-  }
-}
-
-resource "aws_vpc_ipam_pool" "test" { # Crear un pool de direcciones IP en el IPAM.
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
-  locale         = var.vpcconfig.vpc[0].region
-}
-
-resource "aws_vpc_ipam_pool_cidr" "test" { # Asignar un rango CIDR al pool de IPs del IPAM.
-  ipam_pool_id = aws_vpc_ipam_pool.test.id
-  cidr         = "172.20.0.0/16"
-}
-
 resource "aws_vpc" "mi_die_vpc" { # Crear una VPC y asignar direcciones IP desde el pool del IPAM.
-  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
-  ipv4_netmask_length = 28
+  for_each = { for entry in var.vpcconfig.vpc : entry.vpc_name => entry }
+  cidr_block = each.value.cidr_block
+  ipv6_cidr_block = each.value.ipv6_cidr_block
+  ipv6_ipam_pool_id = each.value.ipv6_ipam_pool_id
+  ipv6_netmask_length = each.value.ipv6_netmask_length
+  ipv6_cidr_block_network_border_group = each.value.ipv6_cidr_block_network_border_group
+  ipv4_ipam_pool_id   = each.value.ipv4_ipam_pool_id
+  ipv4_netmask_length = each.value.ipv4_netmask_length
 
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  instance_tenancy     = "default"
+  enable_network_address_usage_metrics = each.value.enable_network_address_usage_metrics
+  enable_dns_support   = each.value.enable_dns_support
+  enable_dns_hostnames = each.value.enable_dns_hostnames
+  instance_tenancy     = each.value.instance_tenancy
 
-  tags = {
-    Name = var.vpcconfig.vpc[0].vpc_name
-  }
+  assign_generated_ipv6_cidr_block = each.value.assign_generated_ipv6_cidr_block
 
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.test
-  ]
+  tags = each.value.tags
+
 }
-
 resource "aws_subnet" "mi_die_subnet" { # Crear múltiples subnets, una por cada Availability Zone, asignándoles bloques CIDR desde el IPAM y etiquetándolas.
   count             = length(var.vpcconfig.vpc[0].availability_zones)
-  vpc_id            = aws_vpc.mi_die_vpc.id
+  vpc_id            = aws_vpc.mi_die_vpc[].id
   cidr_block        = cidrsubnet(aws_vpc_ipam_pool_cidr.test.cidr, 8, count.index)
   availability_zone = var.vpcconfig.vpc[0].availability_zones[count.index]
 
@@ -60,6 +46,5 @@ resource "aws_route_table" "die_route_table" { # Crear una tabla de rutas para c
 resource "aws_route_table_association" "mi_route_table_assoc" { # # Asocia cada subnet con la tabla de rutas para definir cómo se enruta el tráfico dentro de cada subnet.
   count          = length(aws_subnet.mi_die_subnet)
   subnet_id      = aws_subnet.mi_die_subnet[count.index].id
-  route_table_id = aws_route_table.die_route_table.id
-}
+  route_table_id = aws_route_table.die_route_table.id}
 
--- modules/aws-vpc/variables.tf
+++ modules/aws-vpc/variables.tf
@@ -4,7 +4,22 @@ variable "vpcconfig" {
     vpc = list(object({
       region             = string
       vpc_name           = string
-      availability_zones = list(string)
+      cidr_block = string
+      ipv6_cidr_block = string
+      ipv6_ipam_pool_id = string
+      ipv6_netmask_length = string
+      ipv6_cidr_block_network_border_group = string
+      ipv4_ipam_pool_id   = string
+      ipv4_netmask_length = number
+
+      enable_network_address_usage_metrics = bool
+      enable_dns_support   = bool
+      enable_dns_hostnames = bool
+      instance_tenancy     = string
+
+      assign_generated_ipv6_cidr_block = bool
+
+      tags = map(string)
     }))
   })
 }
